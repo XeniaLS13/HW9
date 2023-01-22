86760519a25c99c47af4bec20a0e8cc3eb03c1ff
XeniaLS13 <kseniavagabova@mail.ru>
2023-01-22 04:42:02 +0300

    Добавлен файл с решением

---
+++ Task1/Program.cs
@@ -0,0 +1,17 @@
+﻿int n = InputInt("Введите натуральное число ");
+int m = 1;
+Console.WriteLine(NaturalNumber(n, m));
+
+int NaturalNumber(int n, int m)
+{ if (n == m)
+    return n;
+    else
+        Console.Write($"{NaturalNumber(n, m + 1)}, ");
+    return m;    
+}
+
+int InputInt(string output)
+{
+    Console.Write(output);
+    return int.Parse(Console.ReadLine());
+}
---
+++ Task2/Program.cs
@@ -0,0 +1,16 @@
+﻿int m = InputInt("Введите M: ");
+int n = InputInt("Введите N: ");
+Console.WriteLine($"Сумма элементов от {m} до {n} = {CountNaturalAmount(m, n)}");
+
+int InputInt(string output)
+{
+    Console.Write(output);
+    return int.Parse(Console.ReadLine());
+}
+
+int CountNaturalAmount(int m, int n)
+{
+    if (m == n)
+        return n;
+    return n + CountNaturalAmount(m, n - 1);
+}
